994b07cea4137bf3566f021891844369f227609b
smelukote <smelukote@gmail.com>
2021-09-15 12:45:36 -0700

    Added DAG files

---
+++ loan_data.py
@@ -0,0 +1,17 @@
+DESTINATION_BUCKET = 'sc-flow-dev-data'
+DESTINATION_DIRECTORY = "transferred"
+
+dag_params = {
+   'dag_id': 'PostgresToCloudStorageExample',
+   'start_date': datetime(2020, 7, 7),
+   'schedule_interval': timedelta(days=1),
+}
+
+with DAG(**dag_params) as dag:
+   move_results = PostgresToGoogleCloudStorageOperator(       task_id="move_results",
+       bucket=DESTINATION_BUCKET,
+       filename=DESTINATION_DIRECTORY + "/{{ execution_date }}" + "/{}.json",
+       sql='''SELECT *, due_date::date - effective_date::date as calc FROM loan_data;''',
+       retries=3,
+       postgres_conn_id="postgres_poc"
+   ) 
